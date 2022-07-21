25cdbbfb90bf2c7ce640628109e301454154cdcb
Lucas Yang <yangchenshin77@gmail.com>
2022-07-21 23:13:24 +0800

    Update @typescript-eslint/member-delimiter-style rule

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -17,7 +17,10 @@ module.exports = {
 
     // TS
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-    '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: { delimiter: 'none' },
+      singleline: { delimiter: 'comma', requireLast: false },
+    }],
     '@typescript-eslint/type-annotation-spacing': ['error', {}],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
