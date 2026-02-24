d48b109ae395a66582aa526e979f24e051e448b4
Lucas Yang <yangchenshin77@gmail.com>
2026-02-24 16:02:53 +0800

    Bump @antfu/eslint-config to v7.4

--- src/configs/node.ts
+++ src/configs/node.ts
@@ -1,8 +1,10 @@
 import type { TypedFlatConfigItem } from '../types'
+import { GLOB_SRC } from '../globs'
 
 export async function node(): Promise<TypedFlatConfigItem[]> {
   return [
     {
+      files: [GLOB_SRC],
       name: 'ycs77/node/rules',
       rules: {
         'node/prefer-global/buffer': ['error', 'always'],
